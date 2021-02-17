85ffee4070329bbbbe240d45d83df0d14de3a6ed
parkcheolu <myqueen1202@gmail.com>
2021-02-17 22:53:50 +0900

    dll: pop

--- src/ds/dll.rs
+++ src/ds/dll.rs
@@ -31,10 +31,75 @@ impl<T> DbList<T> {
                     next: Some(e.clone()),
                     prev: None,
                 }));
-                e.borrow_mut().prev = Some(Rc::downgrade(&new_front));
+                let mut me = e.borrow_mut();
+                me.prev = Some(Rc::downgrade(&new_front));
                 self.first = Some(new_front);
             },
-            None => {},
+            None => {
+                let new_data = Rc::new(RefCell::new(DbNode {
+                    data,
+                    next: None,
+                    prev: None,
+                }));
+                self.last = Some(Rc::downgrade(&new_data));
+                self.first = Some(new_data);
+            },
         }
+    }
+
+    pub fn push_back(&mut self, data: T) {
+        match self.last.take() {
+            Some(l) => {
+                let new_back = Rc::new(RefCell::new(DbNode {
+                    data,
+                    next: None,
+                    prev: Some(l.clone()),
+                }));
+                let st = Weak::upgrade(&l).unwrap();
+                let mut ml = st.borrow_mut();
+                self.last = Some(Rc::downgrade(&new_back));
+                ml.next = Some(new_back);
+            },
+            None => {
+                let new_data = Rc::new(RefCell::new(DbNode {
+                    data,
+                    next: None,
+                    prev: None,
+                }));
+                self.last = Some(Rc::downgrade(&new_data));                
+                self.first = Some(new_data);
+            },
+        }
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        match self.first.take() {
+            Some(f) => {
+                match Rc::try_unwrap(f) {
+                    Ok(rc) => Some(rc.into_inner().data),
+                    Err(_) => None
+                }
+            },
+            None => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push_front() {
+        let mut dll = DbList::new();
+        dll.push_front(1);
+        dll.push_front(2);
+        dll.push_back(3);
+        dll.push_back(4);
+        dll.push_front(1);
+
+        let front_val = dll.pop().unwrap();
+        println!("{:?}", front_val);
+        println!("{:?}", dll);
     }
 }
